1832fd0d6b825bb66499906248f947b8a7ec5da1
Mark Seneca <105525017+Joseph-Mogul@users.noreply.github.com>
2022-06-16 10:08:39 +0930

    continue with data functionality, remove dialogs, add expense window formatting, fix table
    Co-authored-by: laranc <laranc@users.noreply.github.com>

--- core/expensewindow.py
+++ core/expensewindow.py
@@ -4,10 +4,6 @@ from tkinter import Tk, Text
 from tkinter import ttk
 from tkinter import PhotoImage
 
-
-# windows
-#import core.dialogs.addexpensewindow as addexpwin
-
 # database
 import tools.datahandler as dh
 
@@ -31,71 +27,98 @@ class expense_window(tk.Tk):
 
         # title label
         self.title_label = ttk.Label(
-            self, text="Expense for " + self.time_raw)
-        self.title_label.grid(row=0, column=4)
-
-        # buttons
-        self.add_expense_window_button = ttk.Button(
-            self, text="Add Expense", command=self.open_add_expense_window_EV)
-        self.add_expense_window_button.grid(row=4, column=4)
+            self, text="Expenses for " + self.time_raw)
+        self.title_label.grid(row=0, column=0)
 
         # table frame
-        expenses_table_frame = ttk.Frame(
+        self.expenses_table = ttk.Frame(
             master=self, relief=tk.SOLID, borderwidth=2)
+        self.expenses_table.grid(row=6, column=1)
 
+        self.expense_table_name_label = ttk.Label(
+            self.expenses_table, text="Name")
+        self.expense_table_name_label.grid(row=0, column=0)
+
+        self.expense_table_value_label = ttk.Label(
+            self.expenses_table, text="Value")
+        self.expense_table_value_label.grid(row=0, column=1) 
         
         self.display_data()
-        # for x in range(2):
-        #     for y in range(2):
-        #         label = ttk.Label(self, text="daasdasdasd")
-        #         label.grid(row=x, column=y)
-
-        expenses_table_frame.grid(row=6, column=5)
-        expenses_table = ttk.Label(self, text="Placeholder")
-        expenses_table.grid(row=0, column=1)
-
+        
         # add expenses frame
         add_expenses_frame = ttk.Frame(
             master=self, relief=tk.SOLID, borderwidth=2)
         add_expenses_frame.grid(row=10, column=1)
 
         # labels
-        self.name_label = ttk.Label(add_expenses_frame, text="Expense Name")
-        self.name_label.grid(row=4, column=4)
+        self.add_expenses_name_label = ttk.Label(add_expenses_frame, text="Expense Name")
+        self.add_expenses_name_label.grid(row=4, column=4)
 
-        self.value_label = ttk.Label(add_expenses_frame, text="Expense Value")
-        self.value_label.grid(row=5, column=4)
+        self.add_expenses_value_label = ttk.Label(add_expenses_frame, text="Expense Value")
+        self.add_expenses_value_label.grid(row=5, column=4)
 
         # text input
-        self.name_input = Text(add_expenses_frame, height=1, width=20)
-        self.name_input.grid(row=4, column=5)
+        self.add_expenses_name_input = Text(add_expenses_frame, height=1, width=20)
+        self.add_expenses_name_input.grid(row=4, column=5)
 
-        self.value_input = Text(add_expenses_frame, height=1, width=20)
-        self.value_input.grid(row=5, column=5)
+        self.add_expenses_value_input = Text(add_expenses_frame, height=1, width=20)
+        self.add_expenses_value_input.grid(row=5, column=5)
 
         # submit data
-        self.add_new_expense = ttk.Button(self, text="Add New Expense", command=self.add_new_expense_EV)
+        self.add_new_expense_button = ttk.Button(self, text="Add New Expense", command=self.add_new_expense_EV)
+        self.add_new_expense_button.grid(row=11, column=1)
         
         
         # remove expenses frame
+        remove_expenses_frame = ttk.Frame(
+            master=self, relief=tk.SOLID, borderwidth=2)
+        remove_expenses_frame.grid(row=10, column=4)
+
+        self.remove_expenses_name_label = ttk.Label(add_expenses_frame, text="Expense Name")
+        self.remove_expenses_name_label.grid(row=4, column=4)
+
+        self.remove_expenses_value_label = ttk.Label(add_expenses_frame, text="Expense Value")
+        self.remove_expenses_value_label.grid(row=5, column=4)
+
+        # text input
+        self.remove_expenses_name_input = Text(add_expenses_frame, height=1, width=20)
+        self.remove_expenses_name_input.grid(row=4, column=5)
+
+        self.remove_expenses_value_input = Text(add_expenses_frame, height=1, width=20)
+        self.remove_expenses_value_input.grid(row=5, column=5)
+
+        # submit data
+        self.remove_new_expense_button = ttk.Button(self, text="Add New Expense", command=self.remove_expense_EV)
+        self.remove_new_expense_button.grid(row=11, column=1)
+
 
 
     def display_data(self) -> None:
         # get json data
+        x = 1
         json_data: list[str] = dh.pull_data(self.time_data[3])
+        print(f"DATA = {json_data}")
         if len(json_data) == 0: # file empty
             pass
         else: # file is full
-            pass
+            for key in json_data[4]:
+                name = ttk.Label(self.expenses_table, text=f"{key}")
+                name.grid(row=x, column=0)
+                value = ttk.Label(self.expenses_table, text=f"{json_data[4][key]}")
+                value.grid(row=x, column=1)
+                x += 1
+
         
     def add_new_expense_EV(self) -> None:
+        name = self.add_expenses_name_input.get(1.0, tk.END+'-1c')
+        value = self.add_expenses_value_input.get(1.0, tk.END+'-1c')
 
-        name = self.name_input.get(1.0, tk.END+"-1c")
-        value = self.value_input.get(1.0, tk.END+"-1c")
-
-        dh.push_new_data()
+        dh.push_data(self.time_data[3], name, value)
         self.display_data()
 
-    # def open_add_expense_window_EV(self) -> None:
-    #     self.add_expense_window = addexpwin.add_expense_window(
-    #         self.time_data, self.time_raw)
+
+    def remove_expense_EV(self) -> None:
+        name = self.remove_expenses_name_input.get(1.0, tk.END+'-1c')
+        value = self.remove_expenses_value_input.get(1.0, tk.END+'-1c')
+
+        self.display_data()
