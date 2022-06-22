9b8dc20e376d6566dc84516b8b0b2a494bfe86d3
laranc <claranjeira@protonmail.ch>
2022-06-22 20:23:11 +0930

    Fixed database integration with sqlite, fixed getting user id from database, TODO: fix json integration

--- tools/user.py
+++ tools/user.py
@@ -2,10 +2,15 @@
 # 'struct' representation of user as existing in the database
 from dataclasses import dataclass
 
+
 @dataclass
 class User():
+    id: int
     firstname: str
     lastname: str
     age: int
     dateofbirth: str
     password: str
+
+
+current_user_id: int
